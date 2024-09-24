e004f41ef9bb6656aec35b345bb9830d2d9961c8
Apriyanto Ulami Setiawan <apriyantoa252@gmail.com>
2024-09-24 09:34:38 +0700

    Update framewo

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const path = require("path");
+const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 
 // Models
@@ -20,16 +21,52 @@ mongoose
 // set view engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/products", async (req, res) => {
-  const products = await Product.find({});
-  console.log(products);
-  res.render("index", { products });
-});
+app
+  .get("/products", async (req, res) => {
+    const products = await Product.find({});
+    console.log(products);
+    res.render("index", { products });
+  })
+  .get("/products/create", (req, res) => {
+    res.render("create.ejs");
+  })
+  .post("/products", async (req, res) => {
+    const product = new Product(req.body);
+    await product.save();
+    res.redirect(`/products/${product._id}`);
+  })
+  .get("/products/:id", async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    console.log(product);
+    if (!product) {
+      return res.status(404).send("Product not found");
+    }
+    res.render("show", { product });
+  })
+  .get("/products/:id/edit", async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render("edit.ejs", { product });
+  })
+  .put("/products/:id", async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true });
+    res.redirect(`/products/${product._id}`);
+  })
+  .delete("/products/:id", async (req, res) => {
+    const { id } = req.params;
+    await Product.findByIdAndDelete(id);
+    res.redirect("/products");
+  });
 
 app.listen(3000, () => {
   console.log(`listening port http://127.0.0.1:${port}`);
